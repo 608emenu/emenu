35e00de4e282986519aeb86918430178e83787ad
leif <leiflyy@outlook.com>
2017-06-08 15:11:24 +0800

    删除了配置文件中的数据库密码信息

--- Public/Conf/config.php
+++ Public/Conf/config.php
@@ -5,7 +5,7 @@
 	define('DB_HOST', 'localhost');
 	define('DB_PORT', '3306');
 	define('DB_USER', 'root');
-	define('DB_PWD', 'toor');
+	define('DB_PWD', '');
 	define('DB_NAME', 'wemall');
 	define('DB_PREFIX', 'emenu_');
 	
